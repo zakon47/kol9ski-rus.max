a60097cb93c023831255555eea65bf1882d7340d
ZAK <060390>
2019-02-24 23:04:48 +0300

    сверстал 1 экран

--- app/page/index.php
+++ app/page/index.php
@@ -59,11 +59,19 @@
             <div class="topline__bar">
                 <i class="fa fa-bars" aria-hidden="true"></i>
             </div>
+            <div class="topline__logo">
+                <a href="/"><i class="svg-icon svg-icon_logo svg-icon_logo-dims"></i></a>
+                <span>Магазин удобных <br>детских колясок</span>
+            </div>
             <div class="topline__phone">
-                тут типо телефон
+                <div class="-email">
+                    <a href="mailto:support@email.com">support@email.com</a>
+                </div>
+                <div class="-phone">
+                    <a href="tel:+74952232323">+7 (495) 223-23-23</a>
+                </div>
             </div>
         </div>
-
     </div>
 </div>
 <div class="navigator -open1">
@@ -71,10 +79,26 @@
         <div class="navigator__black"></div>
         <div class="navigator__wrap">
             <div class="navigator__logo">
-                <a href="/"><i class="svg-icon svg-icon_logo2 svg-icon_logo2-dims"></i></a>
+                <a href="/"><i class="svg-icon svg-icon_logo svg-icon_logo-dims"></i></a>
+                <span>Магазин удобных <br>детских колясок</span>
             </div>
-            <div class="navigator__search">ПОИСК!</div>
-            <div class="navigator__cart">xxaaaaaaaaaaa21a</div>
+            <div class="navigator__search">
+                <form action="/" method="post">
+                    <input type="search" placeholder="Поиск">
+                    <button type="submit"><i class="fa fa-search" aria-hidden="true"></i></button>
+                </form>
+            </div>
+            <div class="navigator__cart">
+                <div class="-wrap">
+                    <a href="/cart">
+                        <div class="svg-icon svg-icon_cart  svg-icon_cart-dims"><b>5</b></div>
+                        <span>1 товар (7 490 р.)</span>
+                    </a>
+                    <button>
+                        <i></i>
+                    </button>
+                </div>
+            </div>
             <div class="navigator__phone">
                 <div class="-email">
                     <a href="mailto:support@email.com">support@email.com</a>
@@ -85,7 +109,7 @@
             </div>
             <div class="navigator__menu">
                 <ul>
-                    <li><a href="/">Главная</a></li>
+                    <li><a href="/" class="-active">Главная</a></li>
                     <li><a href="/catalog">Каталог</a></li>
                     <li><a href="/dostavka">Доставка и оплата</a></li>
                     <li><a href="/contacts">Контакты</a></li>
@@ -94,7 +118,176 @@
         </div>
     </div>
 </div>
+<div class="slider ten">
+    <div class="fx">
+        <div class="slider__wrap">
+            <div class="slider__info">
+                <h1>Самая легкая коляска</h1>
+                <div class="slider__img2">
+                    <img src="/img/slider/kol9ska.png" class="img-responsive">
+                </div>
+                <ul class="-desc zli">
+                    <li>Можно взять в ручную кладь</li>
+                    <li>Коляска очень легкая</li>
+                    <li>Не занимает много места</li>
+                    <li>Помещается в любой багажник</li>
+                </ul>
+                <a href="/catalog" class="btn -big">Перейти в каталог</a>
+            </div>
+            <div class="slider__img">
+                <img src="/img/slider/kol9ska.png" class="img-responsive">
+            </div>
+        </div>
+    </div>
+</div>
+<div class="catalog">
+    <div class="fx">
+        <div class="catalog__wrap">
+            <div class="catalog__title">
+                <h2>Каталог детских колясок YOYA</h2>
+                <div class="volna svg-icon svg-icon_volna svg-icon_volna-dims"></div>
+            </div>
+            <div class="catalog__list">
+                <div>
+                    <div class="catalog__item">
+                        <div class="-img">
+                            <a href="/">
+                                <img src="/img/catalog/1.jpg" class="img-responsive">
+                            </a>
+                        </div>
+                        <div class="-wrap">
+                            <div class="-name">Коляска YOYA 175</div>
+                            <div class="-desc">
+                                <p>Цвет: бежевый</p>
+                            </div>
+                            <div class="-price">
+                                <button class="btn">В корщину</button>
+                                <span>7 490 р.</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div class="catalog__item">
+                        <div class="-img">
+                            <a href="/">
+                                <img src="/img/catalog/1.jpg" class="img-responsive">
+                            </a>
+                        </div>
+                        <div class="-wrap">
+                            <div class="-name">Коляска YOYA 175</div>
+                            <div class="-desc">
+                                <p>Цвет: бежевый</p>
+                            </div>
+                            <div class="-price">
+                                <button class="btn">В корщину</button>
+                                <span>7 490 р.</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div class="catalog__item">
+                        <div class="-img">
+                            <a href="/">
+                                <img src="/img/catalog/1.jpg" class="img-responsive">
+                            </a>
+                        </div>
+                        <div class="-wrap">
+                            <div class="-name">Коляска YOYA 175</div>
+                            <div class="-desc">
+                                <p>Цвет: бежевый</p>
+                            </div>
+                            <div class="-price">
+                                <button class="btn">В корщину</button>
+                                <span>7 490 р.</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div class="catalog__item">
+                        <div class="-img">
+                            <a href="/">
+                                <img src="/img/catalog/1.jpg" class="img-responsive">
+                            </a>
+                        </div>
+                        <div class="-wrap">
+                            <div class="-name">Коляска YOYA 175</div>
+                            <div class="-desc">
+                                <p>Цвет: бежевый</p>
+                            </div>
+                            <div class="-price">
+                                <button class="btn">В корщину</button>
+                                <span>7 490 р.</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div class="catalog__item">
+                        <div class="-img">
+                            <a href="/">
+                                <img src="/img/catalog/1.jpg" class="img-responsive">
+                            </a>
+                        </div>
+                        <div class="-wrap">
+                            <div class="-name">Коляска YOYA 175</div>
+                            <div class="-desc">
+                                <p>Цвет: бежевый</p>
+                            </div>
+                            <div class="-price">
+                                <button class="btn">В корщину</button>
+                                <span>7 490 р.</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div class="catalog__item">
+                        <div class="-img">
+                            <a href="/">
+                                <img src="/img/catalog/1.jpg" class="img-responsive">
+                            </a>
+                        </div>
+                        <div class="-wrap">
+                            <div class="-name">Коляска YOYA 175</div>
+                            <div class="-desc">
+                                <p>Цвет: бежевый</p>
+                            </div>
+                            <div class="-price">
+                                <button class="btn">В корщину</button>
+                                <span>7 490 р.</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div class="catalog__item">
+                        <div class="-img">
+                            <a href="/">
+                                <img src="/img/catalog/1.jpg" class="img-responsive">
+                            </a>
+                        </div>
+                        <div class="-wrap">
+                            <div class="-name">Коляска YOYA 175</div>
+                            <div class="-desc">
+                                <p>Цвет: бежевый</p>
+                            </div>
+                            <div class="-price">
+                                <button class="btn">В корщину</button>
+                                <span>7 490 р.</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
+
+<br><br><br><br><br><br><br><br><br>
 <div class="fx">
 
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing
@@ -136,8 +329,8 @@
             };
             this.open = function(name){
                 this.add(name);                 //добавляем в стек
-                $(name).addClass('-open');      //открываем окно
-                this.body.addClass('ovh');
+                $(name).toggleClass('-open');      //открываем окно
+                this.body.toggleClass('ovh');
             };
             this.close = function(name){
                 // this.remove(name);                 //добавляем в стек
